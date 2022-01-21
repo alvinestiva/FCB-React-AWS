07352c62e9152330db78a0015fc7e79519e107ea
AlvinEstiva <alvin.estiva@tuitt.com>
2022-01-21 14:02:01 +0800

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Task(props){
+  return (
+    <li className="tasks-item">{props.taskName}</li>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    );
+}
+
+function TasksList(){
+  const taskItemsList = [
+      "Follow Edukashon.ph on Facebook",
+      "Follow AWS Siklab Pilipinas on Facebook",
+      "Follow Zuitt Coding Bootcamp on Instagram"
+  ];
+
+  return (
+    <div>
+      <input className="task-input" />
+      <ul>
+          {taskItemsList.map((task, index) => {
+            console.log(task, index);
+            return <Task key={index} taskName={task} />
+          })}
+      </ul>
+    </div>
+    
+  )
+}
+
+function App() {
+
+	const paraStyle = {
+		"marginBottom": "15px"
+	};
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>My Amazing ToDo-List App</h1>
+        <p style={paraStyle}>The most simple and amazing todo-list React app.</p>
+        <TasksList />
+
+      </header>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
